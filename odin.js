9ac75b72b484de555ca4540d2df3adee558a66e4
Simmons9913 <149313756+Simmons9913@users.noreply.github.com>
2023-11-23 15:43:18 -0600

    Add files via upload

---
+++ odin.js
@@ -0,0 +1,76 @@
+let message = "c'mon!";
+const declare = "yesSir!";
+// let person = "Joshua", 
+//   age = "45"
+//   address = "220 Travis";
+
+num = "5";
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_BLUE;
+color = COLOR_GREEN;
+color = COLOR_ORANGE;
+
+// let name = "John";
+// let admin = "John";
+// alert (admin);
+
+// let admin, name;
+// name = "Joshua";
+// admin = name;
+// alert (admin);
+
+// let ourPlanet, currentVisitor;
+
+// let x = 100 + 50;
+// alert (x);
+let a = 45;
+let b = 45;
+let x = a + b + 10;
+
+x = 100 + 50 * a;
+
+let q = 10;
+q--;
+let w = q;
+
+let y = 5;
+let z = y ** 2;
+
+let r = x ** 2;
+
+// function thisFunction () {
+//   return "one more time";
+// }
+// document.getElementById("demo").innerHTML = thisFunction ();
+
+function thisFunction() {
+  return "one more time";
+}
+
+const demoElement = document.getElementById("demo");
+demoElement.innerHTML += thisFunction();
+
+
+const person = {
+  name : "Joshua",
+  age : "45",
+};
+// alert (person.name + " is " + person.age);
+console.log (thisFunction);
+
+function anotherFunction(){
+  return r;
+}
+
+function redirectToPage() {
+  window.location.href = "dolovin.jpg";
+}
+
+function secondFunction() {
+  window.location.href = 'gotcha.html';
+}
